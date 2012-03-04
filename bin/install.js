acee8ed2304f95b9e676c5c82996764ebd216eac
Joshua Holbrook <josh.holbrook@gmail.com>
2012-03-04 11:12:17 -0800

    [refactor] Added download and unzip methods to grabthar (soon to be its own module)

--- bin/install.js
+++ bin/install.js
@@ -3,120 +3,39 @@
 var fs = require('fs'),
     path = require('path'),
     colors = require('colors'),
-    zip = require('zip'),
     utile = require('utile'),
-    request,
     app = require('../lib/grabthar');
 
 app.init();
 
-request = require('request');
+app.download({
+  name: 'craftbukkit',
+  url: 'http://dl.bukkit.org/latest-rb/craftbukkit.jar',
+  path: path.resolve(path.join(__dirname, '..', 'craftbukkit.jar'))
+}, function (err) {
+  if (err) {
+    throw err;
+  }
 
-function getCraftBukkit (next) {
-  var craftbukkit = {
-    'url': 'http://dl.bukkit.org/latest-rb/craftbukkit.jar',
-    'path': path.resolve(path.join(__dirname, '..', 'craftbukkit.jar'))
-  };
-
-  app.log.info('Downloading ' + 'craftbukkit'.cyan + '...');
-
-  app.ticker(function (t) {
-
-    request(craftbukkit.url)
-      .pipe(fs.createWriteStream(craftbukkit.path))
-      .on('close', function (err) {
-        if (err) {
-          throw err;
-        }
-
-        t.stop(function () {
-
-          app.log.info('Downloading ' + 'craftbukkit'.cyan + ' complete.');
-
-          next();
-
-        })
-      });
-    ;
-    t.start();
-  });
-}
-
-function getJSONApi (next) {
-  var api = {
-    'url': 'http://alecgorge.com/minecraft/jsonapi/version/latest/',
-    'path': path.resolve(path.join(__dirname, '..', 'plugins', 'jsonapi.zip'))
-  };
-
-  app.log.info('Downloading ' + 'JSONApi'.cyan + '...');
-
-  utile.mkdirp(path.dirname(api.path), 0775, function (err) {
+  app.download({
+    name: 'JSONApi',
+    url: 'http://alecgorge.com/minecraft/jsonapi/version/latest/',
+    path: path.resolve(path.join(__dirname, '..', 'plugins', 'jsonapi.zip'))
+  }, function (err, state) {
     if (err) {
       throw err;
     }
 
-    app.ticker(function (t) {
-
-      request(api.url)
-        .pipe(fs.createWriteStream(api.path))
-        .on('close', function (err) {
-          if (err) {
-            throw err;
-          }
-
-          t.stop(function () {
-
-            app.log.info('Downloading ' + 'JSONApi'.cyan + ' complete.');
-
-            app.log.info('Unpacking ' + api.path.cyan );
-
-            // This api is all sync but whatever.
-            var files = zip.Reader(fs.readFileSync(api.path)).toObject('utf8');
-
-            utile.async.forEachSeries(Object.keys(files), function (fname, cb) {
-              var p = path.resolve(path.dirname(api.path), fname);
-
-              app.log.info('Writing '.blue + p);
-              utile.mkdirp(path.dirname(p), 0775, function (err) {
-                if (err) {
-                  throw err;
-                }
-
-                fs.writeFileSync(
-                  p,
-                  files[fname]
-                );
-
-                cb();
-              })
-            }, function (err) {
-              if (err) {
-                throw err;
-              }
-
-              app.log.info('Unpacked.');
-              app.log.info('Removing zip archive...');
-
-              utile.rimraf(api.path, function (err) {
-                if (err) {
-                  throw err;
-                }
-
-                next();
-              });
-
-            });
-          });
-        });
-
-      t.start();
-    });
+    app.unzip(state, finish);
   });
-}
 
-getCraftBukkit(function () {
-  getJSONApi(function () {
-    app.log.info('Done.');
-    process.exit(0);
-  });
 });
+
+function finish (err) {
+  if (err) {
+    throw err;
+  }
+
+  app.log.info('Done.');
+  process.exit(0);
+}
