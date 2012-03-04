ec55a2669f2cbb91bbf173cba9e6f479f6176d16
Joshua Holbrook <josh.holbrook@gmail.com>
2012-03-03 19:40:34 -0800

    [dist] Also install the jsonapi plugin

--- bin/install.js
+++ bin/install.js
@@ -3,6 +3,8 @@
 var fs = require('fs'),
     path = require('path'),
     colors = require('colors'),
+    zip = require('zip'),
+    utile = require('utile'),
     request,
     app = require('../lib/grabthar');
 
@@ -36,6 +38,75 @@ function getCraftBukkit (next) {
   app.ticker.start();
 }
 
+function getJSONApi (next) {
+  var api = {
+    'url': 'http://alecgorge.com/minecraft/jsonapi/version/latest/',
+    'path': path.resolve(path.join(__dirname, '..', 'plugins', 'jsonapi.zip'))
+  };
+
+  app.log.info('Downloading ' + 'JSONApi'.cyan + '...');
+
+  var url = 'https://cloud.github.com/downloads/alecgorge/jsonapi/JSONAPI%20v3.4.3.zip';
+
+  request({
+    url: api.url
+  })
+    .pipe(fs.createWriteStream(api.path))
+    .on('close', function (err) {
+    if (err) {
+      throw err;
+    }
+
+    app.ticker.stop();
+
+    app.log.info('Downloading ' + 'JSONApi'.cyan + ' complete.');
+
+    app.log.info('Unpacking ' + api.path.cyan );
+
+    // This api is all sync but whatever.
+    var files = zip.Reader(fs.readFileSync(api.path)).toObject('utf8');
+
+    utile.async.forEachSeries(Object.keys(files), function (fname, cb) {
+      var p = path.resolve(path.dirname(api.path), fname);
+
+      app.log.info('Writing '.blue + p);
+      utile.mkdirp(path.dirname(p), 0775, function (err) {
+        if (err) {
+          throw err;
+        }
+
+        fs.writeFileSync(
+          p,
+          files[fname]
+        );
+
+        cb();
+      })
+    }, function (err) {
+      if (err) {
+        throw err;
+      }
+
+      app.log.info('Unpacked.');
+      app.log.info('Removing zip archive...');
+
+      utile.rimraf(api.path, function (err) {
+        if (err) {
+          throw err;
+        }
+
+        next();
+      });
+
+    });
+
+  });
+
+  app.ticker.start();
+}
+
 getCraftBukkit(function () {
-  app.log.info('Done.');
+  getJSONApi(function () {
+    app.log.info('Done.');
+  });
 });
